35c876b9325e43a7cee1a2181e2a7bad7414c11d
Andy Lee <andylizf@outlook.com>
2023-01-04 22:47:51 +0800

    competitive select.cpp

--- select.cpp
+++ select.cpp
@@ -1,34 +1,32 @@
-#include <chrono>
 #include <format>
 #include <iostream>
 
 #include "bot.hpp"
+#include "game.hpp"
 
 using namespace std;
-using namespace std::chrono;
 
 #ifdef SELECT
+
+template <typename T>
+double battle(int cnt, T&& player1, T&& player2)
+{
+    auto win_cnt = ranges::count_if(views::iota(0, cnt), [player1, player2](auto _) {
+        Contest contest(player1, player2);
+        while (contest.play())
+            ;
+        return contest.winner == 1;
+    });
+    return (double)win_cnt / cnt;
+}
+
 int main()
 {
     double eps = 0.1;
-    for (double C = 0.1; C <= 2; C += eps) {
-        cout << format("Case C={}:", C) << endl;
-        auto mcts_bot_player = mcts_bot_player_generator(C);
-        int win_cnt = 0;
-        int cnt = 20;
-        for (int i = 0; i < cnt; i++) {
-            Contest contest(random_bot_player, mcts_bot_player);
-
-            while (contest.play())
-                ;
-            // cout << endl;
-            if (contest.winner == -1)
-                win_cnt++, cout << "WIN" << endl;
-            else
-                cout << "LOSE" << endl;
-        }
-        double rate = (double)win_cnt / cnt;
-        cout << "Rate: " << rate << endl;
+    for (double C = 1; C <= 2; C += eps) {
+        cout << format("Case C={:.1f}:", C);
+        double rate = battle(10, mcts_bot_player_generator(C), mcts_bot_player_generator(C + eps));
+        cout << format("{:.1f}", rate) << endl;
     }
 }
 #endif
